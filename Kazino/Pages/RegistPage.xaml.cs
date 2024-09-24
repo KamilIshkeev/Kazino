e899b87b773c4057d719ae93455ce661a1e9befd
Rasimqa <gizatullinunga200@gmail.com>
2024-09-24 09:21:07 +0300

    крут

--- Kazino/Pages/RegistPage.xaml.cs
+++ Kazino/Pages/RegistPage.xaml.cs
@@ -69,6 +69,7 @@ namespace Kazino.Pages
                 connect.db.SaveChanges();
                 MessageBox.Show("Пользователь зарегистрирован");
                 _mainWindow.MainFrame.NavigationService.Navigate(new MainPage(_mainWindow));
+
             }
 
         }
